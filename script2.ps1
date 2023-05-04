acbf79ffe492803f9860ffc7e03b6862282e85a5
Eyal Sharvit <131967093+eyal-sharvit-wdc@users.noreply.github.com>
2023-05-04 18:13:24 +0300

    Create script2.ps1

---
+++ script2.ps1
@@ -0,0 +1,9 @@
+$computers = "ULS-DP-SQLCDW","10.24.9.46"
+
+foreach ($computer in $computers) {
+    if (Test-Connection $computer -Quiet) {
+        Write-Host "$computer is online"
+    } else {
+        Write-Host "$computer is offline"
+    }
+}
