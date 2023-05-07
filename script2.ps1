aa2c6c240bbe7268a2fd89f066af0e268473112e
Eyal Sharvit <131967093+eyal-sharvit-wdc@users.noreply.github.com>
2023-05-07 09:25:32 +0300

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -1,5 +1,5 @@
 $computers = "ULS-DP-SQLCDW", "10.24.9.46"
-
+Write-Host "Congratulations! Your first script executed successfully"
 foreach ($computer in $computers) {
     if (Test-Connection $computer -Quiet) {
         Write-Host "$computer is online"
