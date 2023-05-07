fb055957f37b57de5a9d61c5a379c486f1ae8709
Eyal Sharvit <131967093+eyal-sharvit-wdc@users.noreply.github.com>
2023-05-07 09:27:09 +0300

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -1,9 +1,4 @@
-$computers = "ULS-DP-SQLCDW"
+$computer = "ULS-DP-SQLCDW"
 Write-Host "Congratulations! Your first script executed successfully"
-foreach ($computer in $computers) {
-    if (Test-Connection $computer -Quiet) {
-        Write-Host "$computer is online"
-    } else {
-        Write-Host "$computer is offline"
-    }
-}
+Test-Connection $computer -Quiet
+
